50833b73af8fb11e09a0a205c25a320bf247916d
NOBEAT32 <twofirearms@gmail.com>
2025-06-30 12:58:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
+import urllib.request
+
+file_url = "https://drive.google.com/uc?export=download&id=1-bBKd7EjkYWcd0ZON3dqjm7tDjTyOYhV"
+file_path = "similarity.pkl"
+
+if not os.path.exists(file_path):
+    print("Downloading similarity.pkl...")
+    urllib.request.urlretrieve(file_url, file_path)
+    print("Download complete.")
+else:
+    print("File already exists.")
+
 import streamlit as st
 import pickle
 import pandas as pd
